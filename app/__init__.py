68a480c22d88ef158f60d96b7866c1f0d3d45ab0
Carlos Ventura <cventura1959@gmail.com>
2024-10-30 18:09:17 -0400

    Apply initial migration

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_socketio import SocketIO
-from flask_migrate import Migrate
+from flask_migrate import Migrate  # Ensure this import is correct
+
 
 # Initialize extensions globally
 db = SQLAlchemy()

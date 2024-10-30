0e12166cae22db0bd518a8c947620132ca0cf90b
Carlos Ventura <cventura1959@gmail.com>
2024-10-30 16:00:03 -0400

    Updated dependencies for stability and compatibility

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+import os
 from app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)  # Or use socketio.run(app) if using SocketIO
+if __name__ == "__main__":
+    # Use the port provided by Heroku or default to 5000 locally
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
